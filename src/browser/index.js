e45e7d79df767c22334817cc1556ea546b697354
Siddharth Krishnan <siddharth007krishnan@rediffmail.com>
2020-11-03 11:34:37 +0530

    added changes

--- src/browser/index.js
+++ src/browser/index.js
@@ -1,17 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import store from '../shared/redux/store'
 import App from '../shared/App'
 
 import './index.css'
-import { applyMiddleware, createStore } from 'redux'
-import thunk from 'redux-thunk'
-
-const preloadedState = JSON.parse(window.__PRELOADED_STATE__)
-
-delete window.__PRELOADED_STATE__
-
-const store = createStore(reducers, applyMiddleware(thunk), preloadedState)
 
 ReactDOM.hydrate(
   <React.StrictMode>
